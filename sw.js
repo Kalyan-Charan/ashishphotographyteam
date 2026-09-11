bf6cda75a12ecf28cd4d53b59051a699958cf58b
@lwaysCharan <105452047+Kalyan-Charan@users.noreply.github.com>
2026-09-11 12:48:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,17 +5,13 @@ const ASSETS_TO_CACHE = [
   './manifest.json'
 ];
 
-// Install Event - Caching basic assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate Event - Clean up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -31,11 +27,8 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// Fetch Event - Serve cached assets when offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
